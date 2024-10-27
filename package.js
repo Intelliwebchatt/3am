d22c5aec194135eadfced69112fbbd93162f7a86
Intelliwebchatt <aslockhart10@gmail.com>
2024-10-27 13:47:47 -0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,22 @@
+{
+  "name": "3am-productions",
+  "version": "1.0.0",
+  "description": "Investigative journalism platform",
+  "main": "src/js/main.js",
+  "scripts": {
+    "dev": "webpack serve --mode development",
+    "build": "webpack --mode production",
+    "start": "webpack serve --open"
+  },
+  "dependencies": {
+    "gsap": "^3.12.0"
+  },
+  "devDependencies": {
+    "webpack": "^5.89.0",
+    "webpack-cli": "^5.1.4",
+    "webpack-dev-server": "^4.15.1",
+    "css-loader": "^6.8.1",
+    "style-loader": "^3.3.3",
+    "file-loader": "^6.2.0"
+  }
+}
